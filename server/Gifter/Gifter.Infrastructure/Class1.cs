89e948323c63391e11cd17d8d2eae0021bdedb3d
Waterball <waterballs12@gmail.com>
2021-07-12 23:54:35 +0100

    Add infrastructure

--- server/Gifter/Gifter.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Gifter.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ server/Gifter/Gifter.Infrastructure/GiftContext.cs
@@ -0,0 +1,21 @@
+﻿using Gifter.Domain.Models;
+using Gifter.Domain.Options;
+using MongoDB.Driver;
+
+namespace Gifter.Infrastructure
+{
+    public class GiftContext
+    {
+        public IMongoCollection<Gift> Gift { get; }
+
+
+        public GiftContext(GiftContextOptions options)
+        {
+            var mongo = new MongoClient(options.DatabaseConnection);
+
+            var db = mongo.GetDatabase("gifter");
+
+            Gift = db.GetCollection<Gift>("gift");
+        }
+    }
+}
