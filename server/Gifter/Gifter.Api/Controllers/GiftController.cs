210795c63e83e91f5f3c07529897cc35c969d541
Waterball <waterballs12@gmail.com>
2021-07-12 23:54:57 +0100

    Add temporary api

---
+++ server/Gifter/Gifter.Api/Controllers/GiftController.cs
@@ -0,0 +1,33 @@
+﻿using Gifter.Domain.Models;
+using Gifter.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftController : ControllerBase
+    {
+        private readonly GiftContext _context;
+
+        public GiftController(GiftContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSharingGift([FromBody] Gift gift)
+        {
+            await _context.Gift.InsertOneAsync(gift)
+                .ConfigureAwait(false);
+
+            return Ok();
+        }
+
+    }
+}
