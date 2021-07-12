52da3792a71f5bef5cf8a56e9832e181725202ac
Waterball <waterballs12@gmail.com>
2021-07-12 23:54:44 +0100

    Add initial domain

---
+++ server/Gifter/Gifter.Domain/Models/Gift.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Gifter.Domain.Models
+{
+    public class Gift
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        [BsonElement("items")]
+        public HashSet<GiftItem> Items { get; set; }
+    }
+}
