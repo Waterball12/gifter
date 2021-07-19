da461332f75df17f2f93a016b78a25983a86f53c
Waterball <waterballs12@gmail.com>
2021-07-19 19:28:35 +0100

    Fix template link

--- frontend/src/templates/Main.tsx
+++ frontend/src/templates/Main.tsx
@@ -181,13 +181,7 @@ const Main = (props: IMainProps) => {
                     <span role="img" aria-label="Love">
                     ♥
                 </span>{' '}
-                    by <a href="https://creativedesignsguru.com">Waterball</a>
-                    {/*
-                 * PLEASE READ THIS SECTION
-                 * We'll really appreciate if you could have a link to our website
-                 * The link doesn't need to appear on every pages, one link on one page is enough.
-                 * Thank you for your support it'll mean a lot for us.
-                 */}
+                    by <a href="https://github.com/Waterball12">Waterball</a>
                 </div>
             </div>
         </div>
